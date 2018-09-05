db47f1cfb0b877e04ea76921e77a55f0648fc7f2
kennethwu <kennethwu@leadstec.com>
2018-09-05 11:58:35 +0800

    Use jsp to display a page instead of using servlet

---
+++ src/main/java/com/kenneth/login/LoginServlet.java
@@ -0,0 +1,24 @@
+package com.kenneth.login;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/login.do")
+public class LoginServlet extends HttpServlet {
+
+	
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/views/login.jsp")
+		.forward(request, response);
+	}
+
+}
