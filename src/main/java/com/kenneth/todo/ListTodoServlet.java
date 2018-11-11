24bf65750ac73ac1ed415514592630bc40868ba3
kylwu <kylwu@ogcio.gov.hk>
2018-11-11 17:10:59 +0800

    Quick test on JDBC Connection before develop the DAO

--- src/main/java/com/kenneth/todo/ListTodoServlet.java
+++ src/main/java/com/kenneth/todo/ListTodoServlet.java
@@ -1,6 +1,11 @@
 package com.kenneth.todo;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -20,10 +25,58 @@ public class ListTodoServlet extends HttpServlet {
 		
 		// JDBC set up for creating DAO
 		String databaseDriverClassName = getServletContext().getInitParameter("databaseDriverClassName");
-		String url= getServletContext().getInitParameter("databaseURL");
-		String uname = getServletContext().getInitParameter("databaseUsername");
-		String pass = getServletContext().getInitParameter("databasePassword");
+		String databaseURL= getServletContext().getInitParameter("databaseURL");
+		String databaseUsername = getServletContext().getInitParameter("databaseUsername");
+		String databasePassword = getServletContext().getInitParameter("databasePassword");
 		
+		
+		Connection con = null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			Class.forName(databaseDriverClassName);
+			
+			con = DriverManager.getConnection(databaseURL, databaseUsername, databasePassword);
+			
+			st = con.prepareStatement("SELECT * FROM todo");
+			
+			rs = st.executeQuery();
+			
+			while(rs.next()) {
+				
+				System.out.println(rs.getInt(1) + " : " + rs.getString(2));			
+				
+			}
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if(rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {					
+					e.printStackTrace();
+				}				
+			}
+			
+			if(st != null) {
+				try {
+					st.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			if(con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 		
 		
 		request.setAttribute("todos", todosService.getTodos());
