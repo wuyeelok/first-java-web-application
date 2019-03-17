4427402f39f88b2a59981aea6c2c2e091a70a913
wuyeelok <wuyeelok@gmail.com>
2019-03-17 20:42:24 +0800

    HTTP GET pass parameter from browser to server, pass attribute to JSP

--- src/main/java/com/in28minutes/LoginServlet.java
+++ src/main/java/com/in28minutes/LoginServlet.java
@@ -20,6 +20,9 @@ public class LoginServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
 		
+		String name = request.getParameter("name");
+		request.setAttribute("name", name);
+		
 		request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);
 		
 	}
--- src/main/webapp/WEB-INF/views/login.jsp
+++ src/main/webapp/WEB-INF/views/login.jsp
@@ -8,5 +8,6 @@
 </head>
 <body>
 	<h1>My First JSP</h1>
+	<p>${name}&nbsp;<%=request.getAttribute("name") %></p>
 </body>
 </html>
